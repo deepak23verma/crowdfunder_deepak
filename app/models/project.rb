602c9f45098fd73b5830fbaa2ce405ae72eb7b43
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-01 21:03:51 -0400

    Seeding db

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,5 @@ class Project < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 100}
   validates :teaser, presence: true, length: {maximum: 255}
   validates :description, presence: true
-  validates :goal, presence: true, numericality: {greater_than 0, allow_blank: true}
+  validates :goal, presence: true, numericality: {greater_than: 0, allow_blank: true}
 end
