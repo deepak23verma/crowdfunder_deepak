6ac0ce866c6f6fa3fb9a0d08fa2d85d780da6922
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-03 20:34:30 -0400

    Pledge test: should require a user passed

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,6 +1,9 @@
 class Pledge < ActiveRecord::Base
   attr_accessible :amount, :project_id, :user_id
 
-  belongs_to :user_id
+  validates :project_id, :user_id, presence: true
+  # validates :amount, numericality: true
+
+  belongs_to :user
   belongs_to :project
 end
--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Pledge do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should require a user" do
+  	pledge = FactoryGirl.build(:pledge, user: nil)
+
+  	# Should not be able to save
+  	pledge.should_not == pledge.save
+
+  	expect(pledge).to have(1).errors_on(:user_id)
+  end
 end
