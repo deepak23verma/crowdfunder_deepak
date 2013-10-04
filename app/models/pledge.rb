40b5793ccf05535d2ce265297136c17db59991c5
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-03 21:25:54 -0400

    Pledge model tests: all tests for the model passed

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,8 +1,8 @@
 class Pledge < ActiveRecord::Base
   attr_accessible :amount, :project_id, :user_id
 
-  validates :user_id, presence: true
-  validates :amount, numericality: true
+  validates :user_id, :project_id, presence: true
+  validates :amount, numericality: {greater_than: 0, only_integer: true}
 
   belongs_to :user
   belongs_to :project
--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -10,10 +10,37 @@ describe Pledge do
   	expect(pledge).to have(1).errors_on(:user_id)
   end
 
+  it "should require a project" do
+  	pledge = FactoryGirl.build(:pledge, project: nil)
+
+  	# Should not be able to save
+  	pledge.should_not == pledge.save
+
+  	expect(pledge).to have(1).errors_on(:project_id)
+  end
+
   it "should require an amount" do
   	pledge = FactoryGirl.build(:pledge, amount: nil)
 
   	# Should not be able to save
+  	pledge.should_not == pledge.save
+
+  	expect(pledge).to have(1).errors_on(:amount)
+  end
+
+  it "should require an amount greater than 0" do
+  	pledge = FactoryGirl.build(:pledge, amount: -10)
+
+  	# Should not be able to save
+  	pledge.should_not == pledge.save
+
+  	expect(pledge).to have(1).errors_on(:amount)
+  end
+
+  it "should require an amount to be an integer" do
+  	pledge = FactoryGirl.build(:pledge, amount: 'abc')
+
+  	# Should not be a able to save
   	pledge.should_not == pledge.save
 
   	expect(pledge).to have(1).errors_on(:amount)
